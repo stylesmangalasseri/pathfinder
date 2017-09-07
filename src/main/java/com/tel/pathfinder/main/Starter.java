1aed7eabb2791d8ee4f3d56886c080c29ccdd7ba
Styles M <Stylesmon_Mangalasseri@cable.comcast.com>
2017-09-07 11:26:46 +0530

    added maven configuration
    Change-Id: I21b1982d33e38038adc4fe93086178676972e01c

---
+++ src/main/java/com/tel/pathfinder/main/Starter.java
@@ -0,0 +1,15 @@
+package com.tel.pathfinder.main;
+
+/**
+ * Starter class
+ * 
+ * @author styles mangalasseri
+ * 
+ */
+public class Starter {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi Welcome");
+	}
+}
